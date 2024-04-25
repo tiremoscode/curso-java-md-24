eeb773f748eeb460cf60b2ee9c94580ad30aad4a
Abigail Tolentino <abigail_2039@hotmail.com>
2024-04-25 17:44:49 -0600

    abigail tolentino calculadora

---
+++ practicas/calculadora/CalculadoraAbigailTolen.java
@@ -0,0 +1,20 @@
+package practicas.calculadora;
+
+public class CalculadoraAbigailTolen {
+        public static void main(String args []){
+
+            //Variables donde haré el calculo.
+            int numero1 = 30;
+            int numero2 = 50;      
+            
+            int suma = numero1 + numero2;
+            int resta = numero1 - numero2;
+            int multiplicacion = numero1 * numero2;
+            int division = numero1 / numero2;
+    
+            System.out.println("El resultado es: " + suma);
+            System.out.println ("El resulado de la resta es: " + resta);
+            System.out.println("El resultado de la multiplicación es: "+ multiplicacion);
+            System.out.println("El resultado de la división es; " + division);
+             }
+ }
