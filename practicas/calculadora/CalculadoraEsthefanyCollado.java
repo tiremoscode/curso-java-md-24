ef07ad8737a22ec3c78749fb76a2d13fef3cbe49
Esthefany Karina Collado Gomez <fany2832m@gmail.com>
2024-04-25 19:50:03 -0600

    agregue cambios en la calculadora

---
+++ practicas/calculadora/CalculadoraEsthefanyCollado.java
@@ -0,0 +1,25 @@
+package practicas.calculadora;
+
+public class CalculadoraEsthefanyCollado {
+    public static void main(String args[]) {
+        // Variables donde haré el calculo.
+        int numero1 = 30;
+        int numero2 = 50;
+
+        // Suma
+        int suma = numero1 + numero2;
+        System.out.println("El valor de la suma " + suma);
+
+        // Resta
+        int resta = numero1 - numero2;
+        System.out.println("El valor de la resta " + resta);
+
+        // Multiplicación
+        int multiplicacion = numero1 * numero2;
+        System.out.println("El valor de la multiplicacion " + multiplicacion);
+
+        // División
+        int division = numero1 / numero2;
+        System.out.println("El valor de la division " + division);
+    }
+}
