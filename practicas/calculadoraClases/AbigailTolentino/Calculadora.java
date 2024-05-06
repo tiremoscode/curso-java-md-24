41629b53255746d073eeac983ae9b8548854aaa3
Abigail Tolentino <abigail_2039@hotmail.com>
2024-05-06 15:26:47 -0600

    CalculadoraClases

---
+++ practicas/calculadoraClases/AbigailTolentino/Calculadora.java
@@ -0,0 +1,21 @@
+package practicas.calculadoraClases.AbigailTolentino;
+
+public class Calculadora {
+    public static void main(String args[]) {
+
+        // Variables donde haré el calculo.
+
+        double num1 = 20.6;
+        double num2 = 30.2;
+        
+        System.out.println("Primer numero" + num1);
+        System.out.println("Segundo numero " + num2);
+       
+        Operaciones ope = new Operaciones();
+        // Suma
+        System.out.println("La suma de " + num1 + " +  " + num2 + " = " + ope.sumar(num1, num2));
+        System.out.println("La resta de " + num1 + " -  " + num2 + " = " + ope.restar(num1, num2));
+        System.out.println("La multiplicación de " + num1 + " *  " + num2 + " = " + ope.multiplicacion(num1, num2));
+        System.out.println("La división de " + num1 + " /  " + num2 + " = " + ope.dividir(num1, num2));
+}
+}
