ba161a90d60b90d39cf3f7ec5d57c0c942d67ce4
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-05-09 16:20:06 -0600

    Se añaden archivos Git.java y Pokemon.java

---
+++ Git.java
@@ -0,0 +1,5 @@
+public class Git {
+    public static void main(String args[]) {
+        System.out.println("Hola Clase de git");
+    }
+}
--- Pokemon.java
+++ Pokemon.java
@@ -2,5 +2,6 @@ public class Pokemon {
     public static void main(String[] args) {
         // Todo - Crear un ejemplo de Pokemon
         // para las Clases y Objetos
+        System.out.println("Hola Pokemon!!!");
     }
 }
