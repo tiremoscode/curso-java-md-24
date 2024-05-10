92985dca4df26de6ac6d82954600f0ae7fd8992f
Dafne Ramos <163070296+daph93@users.noreply.github.com>
2024-05-09 19:50:20 -0600

    Create CalculadoraDafneRamos.java

---
+++ practicas/calculadora/CalculadoraDafneRamos.java
@@ -0,0 +1,25 @@
+package practicas.calculadora;
+
+public class CalculadoraDafneRamos {
+    public static void main(String args[]) {
+        // Variables donde haré el calculo.
+        int numero1 = 70;
+        int numero2 = 20;
+
+        // Suma
+        int suma = numero1 + numero2;
+        System.out.println("El valor de la suma " + suma);
+
+        // Resta
+        int resta = numero1 - numero2;
+        System.out.println("El valor de la resta" + resta);
+
+        // Miltiplicacion
+        int multiplicacion = numero1 * numero2;
+        System.out.println("El valor de la multiplicacion" + multiplicacion);
+
+        // Divicion
+        int divicion = numero1 / numero2;
+        System.out.println("El valor de la divicion" + divicion);
+    }
+}
