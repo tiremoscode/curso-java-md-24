5c8e97d8e04403ef80614703ff14a90d60010264
VSALINASP94 <VSALINASP94@GMAIL.COM>
2024-05-12 15:56:46 -0600

    practica calculadora por clases Veronica Salinas Padilla

---
+++ practicas/calculadoraClases/VeronicaS/Calculadora.java
@@ -0,0 +1,22 @@
+package practicas.calculadoraClases.VeronicaS;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        double numero1=10;
+        double numero2 =10;
+
+        Operaciones ope = new Operaciones();
+       // Suma
+       System.out.println("Suma de " + numero1 + " +  " + numero2 + " es igual a " + ope.sumar(numero1, numero2));
+
+       System.out.println("resta de " + numero1 + " +  " + numero2 + " es igual a " + ope.restar(numero1, numero2));
+
+       System.out.println("multiplicacion de " + numero1 + " +  " + numero2 + " es igual a " + ope.multiplcar(numero1, numero2));
+
+       System.out.println("división  de " + numero1 + " +  " + numero2 + " es igual a " + ope.dividir(numero1, numero2));
+
+
+    }
+    
+}
