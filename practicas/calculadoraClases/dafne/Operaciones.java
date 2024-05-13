b605fe95af4613ec512bb0fc93f4edff0b4251d4
DafneRamos <daphner27@gmail.com>
2024-05-13 17:32:37 -0600

    CalculadoraDafne

---
+++ practicas/calculadoraClases/dafne/Operaciones.java
@@ -0,0 +1,17 @@
+package practicas.calculadoraClases.Dafne;
+
+public class Operaciones {
+    public double sumar(double num1, double num2) {
+        return num1 + num2;
+    }
+    public double restar (double num1, double num2){
+        return num1 - num2;
+    }
+    public double multiplicacion (double num1, double num2){
+        return num1 * num2;
+    }
+    public double dividir (double num1, double num2){
+        return num1 / num2;
+    }
+    
+}
