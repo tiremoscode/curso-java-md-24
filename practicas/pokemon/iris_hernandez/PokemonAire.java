7ec32daae34985584278e4aac7c9dcb7cba966f1
Alba Iris Hernandez Ortega <irisher955@gmail.com>
2024-05-13 20:53:28 -0600

    Agrego pokemones aire y electricos

---
+++ practicas/pokemon/iris_hernandez/PokemonAire.java
@@ -0,0 +1,23 @@
+package practicas.pokemon.iris_hernandez;
+
+class PokemonAire extends Pokemon implements Elemento {
+    public PokemonAire(String nombre, int nivel, int edad) {
+        super(nombre, nivel, edad);
+    }
+
+    @Override
+    public String getElementType() {
+        return "Aire";
+    }
+
+    @Override
+    public int getEdad() {
+        return 17;
+    }
+
+    @Override
+    public void atacar() {
+        super.atacar();
+        System.out.println(getNombre() + " lanza tornado!");
+    }
+}
