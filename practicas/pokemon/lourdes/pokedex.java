cfa32d689b096b91644efdaf6284d2e87f7a8a90
Lourdes Dianey Robles Alvarado <lourdesdianey_robles@comunidad.unam.mx>
2024-05-13 23:13:05 -0600

    para tener algo avanzado.

--- practicas/pokemon/lourdes/pokedex.java
+++ practicas/pokemon/lourdes/pokedex.java
@@ -1,13 +1,2 @@
-package practicas.pokemon.lourdes;
 
-public class pokedex {
-    public static void main(String[] args) {
-        pokemonAgua corviknight = new PokemonAgua ("Corviknight", 50, 17);
-        pokemonElectro electrike = new PokemonElectro("Electrike", 50, 25);
-        pokemonElectro luxray = new pokemonElectro ();
 
-        corviknight.atacar();
-        electrike.atacar();
-        luxray.atacar();
-    }
-}
