4109a5767db96fc01769426f9a06afa2c6fbb879
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-05-13 18:21:08 -0600

    :sparkles: Herencia Interfaces Pokemon - 13 Mayo 2024

--- Pokemon.java
+++
@@ -1,9 +0,0 @@
-public class Pokemon {
-    public static void main(String[] args) {
-        // Todo - Crear un ejemplo de Pokemon
-        // para las Clases y Objetos
-        System.out.println("Hola Pokemon!!!");
-        String pikachu = "Soy pikachu";
-        System.out.println(pikachu);
-    }
-}
---
+++ ejerciciosClase/Pokemon/Pokedex.java
@@ -0,0 +1,11 @@
+package ejerciciosClase.Pokemon;
+
+public class Pokedex {
+    public static void main(String[] args) {
+        PokemonFuego charmander = new PokemonFuego("Charmander", 50, 15);
+        PokemonAgua squirtle = new PokemonAgua("Squirtle", 50, 15);
+
+        charmander.atacar();
+        squirtle.atacar();
+    }
+}
