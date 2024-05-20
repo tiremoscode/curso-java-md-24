7a6c8022b2016f6f6c97c3ce0fcfd1bdf1447a55
Jessica Nohemy Silva Castro <jessica.nohemy.14.05.96@gmail.com>
2024-05-19 21:28:12 -0600

    Agregue clases

---
+++ practicas/calculadora/CalculadoradeClases/CalculadoraClasesJessicaSilva.java
@@ -0,0 +1,24 @@
+package practicas.calculadora.CalculadoradeClases;
+
+public class CalculadoraClasesJessicaSilva {
+
+    public static void main(String[] args) {
+        // Variables
+        double numero1 = 29.9, numero2 = 9.9;
+
+        Operaciones op = new  Operaciones();
+        
+        //suma
+        System.out.println("La suma de " + num1 + " +  " + num2 + " = " + ope.sumar(num1, num2));
+        // resta
+        System.out.println("La resta de " + num1 + " -  " + num2 + " = " + ope.restar(num1, num2));
+        // multiplicacion 
+
+        System.out.println("La multiplicación de " + num1 + " *  " + num2 + " = " + ope.multiplicacion(num1, num2));
+        // division 
+        System.out.println("La división de " + num1 + " /  " + num2 + " = " + ope.dividir(num1, num2));
+
+    }
+
+    }
+    
