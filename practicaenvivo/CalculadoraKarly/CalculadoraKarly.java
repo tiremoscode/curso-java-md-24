afbff32f4f084cbf55e10db6e7b4b166036af871
Karla Andrea Cardenas Bonifacio <karlacard95@gmail.com>
2024-05-22 18:49:03 -0600

    karla cardenas calculadora

---
+++ practicaenvivo/CalculadoraKarly/CalculadoraKarly.java
@@ -0,0 +1,15 @@
+package CalculadoraKarly;
+
+public class CalculadoraKarly {
+    
+    public static void main(String args []){
+        //Variables donde haré el calculo.
+        int numero1 = 30;
+        int numero2 = 50;      
+        
+        // Suma
+        int suma = numero1 + numero2;
+        System.out.println("El valor de la suma "+suma);
+
+    }
+}
