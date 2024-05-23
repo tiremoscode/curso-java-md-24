cc11fd69854b8dd290fd6d898f86acbabac85806
reyxangel <deynahy@gmail.com>
2024-05-23 11:51:48 -0600

    Actualización

--- practicas/calculadoraClases/Rey-Angeles/Calculadora.java
+++ practicas/calculadoraClases/Rey-Angeles/Calculadora.java
@@ -1,27 +1,30 @@
-public class Calculadora {
+package practicas.calculadoraClases.Calculadora.Rey-Angeles {
     public static void main(String args []) {
         // Variables donde haré el cálculo.
-        int numero1 = 30;
-        int numero2 = 50;      
+        double numero1 = 1.7;
+        double numero2 = 9.3;      
         
+        // Objeto:
+        Operaciones op = new Operaciones();
+
         // Suma
+        double suma = numero1 + numero2;
+        System.out.println("Suma: " + suma);
         
-        int suma = numero1 + numero2;
-        System.out.println("El valor de la suma: " + suma);
-
         // Resta
-
-        int resta = numero1 - numero2;
-        System.out.println("El valor de la resta: " + resta);
+        double resta = numero1 - numero2;
+        System.out.println("Resta: " + resta);
 
         // Multiplicación
-
-        int multiplicacion = numero1 * numero2;
-        System.out.println("El valor de la multiplicación: " + multiplicacion);
+        double multiplicacion = numero1 * numero2;
+        System.out.println("Multiplicación: " + multiplicacion);
 
         // División
-
-        double division = (double) numero1 / numero2;
-        System.out.println("El valor de la división: " + division);
+        if (numero2 != 0) {
+            double division = numero1 / numero2;
+            System.out.println("División: " + division);
+        } else {
+            System.out.println("Error: División por cero.");
+        }
     }
 }
