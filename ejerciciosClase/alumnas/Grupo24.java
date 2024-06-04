86160773dc9c57f8b4473c2f4d763d4632fa5c72
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-06-03 18:25:15 -0600

    :sparkles: Métodos leer e insertar alumna

--- ejerciciosClase/alumnas/Grupo24.java
+++ ejerciciosClase/alumnas/Grupo24.java
@@ -1,6 +1,7 @@
 package ejerciciosClase.alumnas;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Grupo24 {
 
@@ -8,9 +9,9 @@ public class Grupo24 {
     // JDBC
     private static String url = "jdbc:mysql://localhost:3306/grupo24";
     private static String usuario = "root";
-    private static String password= "buttons";
+    private static String password = "buttons";
 
-    public void obtenerAlumnos(){
+    public void obtenerAlumnos() {
         String query = "SELECT * FROM alumnas";
         try (Connection connection = DriverManager.getConnection(url, usuario, password);
                 Statement statement = connection.createStatement();
@@ -26,13 +27,63 @@ public class Grupo24 {
 
         } catch (Exception e) {
             e.printStackTrace();
-        }  
+        }
+    }
+
+    public static void guardarAlumna(String nombre, int edad) {
+        String queryInsertar = "INSERT INTO alumnas (nombre, edad) VALUES (?, ?)";
+        try (Connection connection = DriverManager.getConnection(url, usuario, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(queryInsertar)) {
+            preparedStatement.setString(1, nombre);
+            preparedStatement.setInt(2, edad);
+
+            int registrosInsertados = preparedStatement.executeUpdate();
+            System.out.println("Registros insertados: " + registrosInsertados);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
         Grupo24 operacionesBD = new Grupo24();
 
-        // Obtener todos los registros de la base de datos.
-        operacionesBD.obtenerAlumnos();
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Seleccionar una opción.
+            System.out.println(
+                    "Ingresa una opción. \n1.- Muestra todos los alumnos.\n2.- Insertar un nuevo Alumno\n3.- Actualizar un Alumno\n4.- Eliminar un Alumno");
+            int menu = scanner.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("*** Mostrar todos los alumnos ***");
+                    operacionesBD.obtenerAlumnos();
+                    break;
+                case 2:
+                    System.out.println("*** Insertar un nuevo alumno ***");
+                    scanner.nextLine();
+                    System.out.println("Ingresa el nombre del alumno: ");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingresa la edad del alumno: ");
+                    int edad = scanner.nextInt();
+                    guardarAlumna(nombre, edad);
+                    break;
+                case 3:
+                    System.out.println("*** Actualizar un alumno ***");
+                    break;
+                case 4:
+                    System.out.println("*** Eliminar un alumno ***");
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+
     }
 }
