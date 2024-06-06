a01e8df99bae4554d80a7c92395e50b322294d33
Lourdes Dianey Robles Alvarado <lourdesdianey_robles@comunidad.unam.mx>
2024-06-06 16:25:25 -0600

    todolisto

---
+++ practicas/calculadoraClases/lourdesrobles/Operaciones.java
@@ -0,0 +1,19 @@
+package practicas.calculadoraClases.lourdesrobles;
+
+public class Operaciones {
+        public double sumar(double numero1, double numero2) {
+            return numero1 + numero2;
+        }
+    
+        public double restar(double numero1, double numero2) {
+            return numero1 - numero2;
+        }
+    
+        public double multiplicar(double numero1, double numero2) {
+            return numero1 * numero2;
+        }
+    
+        public double dividir(double numero1, double numero2) {
+            return numero1 / numero2;
+        }
+    }
