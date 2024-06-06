c9bcd4e3e03853e590bd38c0d68cb590cf5ab782
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-06-06 17:16:33 -0600

    :sparkles: CRUD Finalizado alumnas

--- ejerciciosClase/alumnas/Grupo24.java
+++ ejerciciosClase/alumnas/Grupo24.java
@@ -7,6 +7,13 @@ public class Grupo24 {
 
     // Parámetros de conexión
     // JDBC
+    // CRUD
+    // Create, Read, Update, Delete
+    // Create - Crea un nuevo registro
+    // Read - Obtiene un registro o n registros
+    // Update - Actualizar un registro con nueva infiormación
+    // Delete - Elimina un registro existente
+
     private static String url = "jdbc:mysql://localhost:3306/grupo24";
     private static String usuario = "root";
     private static String password = "buttons";
@@ -43,7 +50,35 @@ public class Grupo24 {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public void eliminarAlumna(int id) {
+        String queryEliminarAlumnas = "DELETE FROM alumnas WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, usuario, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(queryEliminarAlumnas)) {
+            preparedStatement.setInt(1, id);
+            int registrosEliminados = preparedStatement.executeUpdate();
+            System.out.println("Registros eliminados: " + registrosEliminados);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizarAlumna(int id, String nombre, int edad) {
+        String queryActualizarAlumna = "UPDATE alumnas SET nombre = ?, edad = ? WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, usuario, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(queryActualizarAlumna)) {
+            preparedStatement.setString(1, nombre);
+            preparedStatement.setInt(2, edad);
+            preparedStatement.setInt(3, id);
+
+            int registrosActualizados = preparedStatement.executeUpdate();
+            System.out.println("Registros actualizados: " + registrosActualizados);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -74,9 +109,25 @@ public class Grupo24 {
                     break;
                 case 3:
                     System.out.println("*** Actualizar un alumno ***");
+
+                    System.out.println("Ingresa el id del alumno a actualizar");
+                    int idAlumnaActualizar = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Ingresa el nuevo nombre de la alumna");
+                    String nombreAlumnaActualizar = scanner.nextLine();
+
+                    System.out.println("Ingresa la nueva edad de la alumna");
+                    int edadAlumnaActualizar = scanner.nextInt();
+
+                    operacionesBD.actualizarAlumna(idAlumnaActualizar, nombreAlumnaActualizar, edadAlumnaActualizar);
+
                     break;
                 case 4:
                     System.out.println("*** Eliminar un alumno ***");
+                    System.out.println("Ingresa el id del alumno a eliminar");
+
+                    int idAlumnaEliminar = scanner.nextInt();
+                    operacionesBD.eliminarAlumna(idAlumnaEliminar);
                     break;
             }
         } catch (Exception e) {
